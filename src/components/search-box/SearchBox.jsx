1f1108cc184396fcecbb0cc53f09e9822c21c606
Harry EMARH <emarh.harry.code@gmail.com>
2024-07-21 15:02:48 +0200

    SearchBox Component updated

--- src/components/search-box/SearchBox.jsx
+++ src/components/search-box/SearchBox.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import './SearchBox.css';
 
-const SearchBox = ({ onSearchHandler }) => {
-  return <input type="search" placeholder="Search monsters" onChange={onSearchHandler}/>;
+const SearchBox = ({ onSearchHandler, placeholder }) => {
+  return <input type="search" placeholder={placeholder} onChange={onSearchHandler}/>;
 };
 
 export default SearchBox;
