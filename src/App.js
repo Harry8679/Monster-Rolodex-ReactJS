b166260a6cb6ee2ab097770a0b1204677844cb00
Harry EMARH <emarh.harry.code@gmail.com>
2024-07-21 15:38:59 +0200

    State and Event added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import './App.css';
-import SearchBox from './components/search-box/SearchBox';
+import { useState } from "react";
+import "./App.css";
+import SearchBox from "./components/search-box/SearchBox";
 
 function App() {
-  const onSearchChange = () => {};
+  const [searchField, setSearchField] = useState('');
+  console.log({ searchField });
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
+  };
 
   return (
     <div className="App">
       <h1 className="app-title bigelow-rules-regular">Monsters Rolodex</h1>
-
-      <SearchBox className='monsters-search-box' onSearchHandler={onSearchChange} placeholder='Search Monsters' />
+      <SearchBox
+        className="monsters-search-box"
+        onSearchHandler={onSearchChange}
+        placeholder="Search Monsters"
+      />
     </div>
   );
 }
